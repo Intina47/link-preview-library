b7ca24af457f88446f05c80d5dbb2225aa00c45a
mamba <isaiahntina47@gmail.com>
2024-06-17 08:13:50 +0000

    chore: Add React app entry point and LinkPreview component

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LinkPreview from './components/LinkPreview';
+import './styles/tailwind.css';
+
+const App = () => (
+    <div className="container mx-auto p-4">
+        <h1 className="text-2xl mb-4">Link Preview Example</h1>
+        <LinkPreview url="https://example.com" />
+    </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
